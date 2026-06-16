5cd8b61cc0aba89d4f0abcbb007cc184617dd656
Olejka45 <oleganiskov45@gmail.com>
2026-06-16 14:04:22 +0300

    Update sw.js

--- static/sw.js
+++ static/sw.js
@@ -1,13 +1,35 @@
-const CACHE_NAME = 'moc-v1';
+const CACHE_NAME = 'moc-v2';
+const urlsToCache = [
+    '/',
+    '/static/style.css',
+    '/static/script.js',
+    '/static/manifest.json'
+];
 
 self.addEventListener('install', event => {
-  self.skipWaiting();
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => cache.addAll(urlsToCache))
+    );
+    self.skipWaiting();
 });
 
 self.addEventListener('fetch', event => {
-  event.respondWith(fetch(event.request));
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => response || fetch(event.request))
+            .catch(() => caches.match('/'))
+    );
 });
 
 self.addEventListener('activate', event => {
-  event.waitUntil(clients.claim());
+    event.waitUntil(
+        caches.keys().then(keys => {
+            return Promise.all(
+                keys.filter(key => key !== CACHE_NAME)
+                    .map(key => caches.delete(key))
+            );
+        })
+    );
+    self.clients.claim();
 });
